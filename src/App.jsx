35b115f6ce39c0afe414e89dd7eb12ceb8ec0c4c
Din <Din.Salehy@triosstudent.com>
2026-03-10 18:18:12 -0400

    Removed LoginGate and allow direct dashboard access for demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import AddPlace from "./components/Places/AddPlaceForm";
 import AddCity from "./components/Cities/AddCityForm";
-import LoginGate from "./components/Authentication/LoginGate";
+// import LoginGate from "./components/Authentication/LoginGate";
 
 export default function App() {
   return (
-    <LoginGate>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/add-place" element={<AddPlace />} />
         <Route path="/add-city" element={<AddCity />} />
         <Route path="/notifications" element={<AddCity />} />
       </Routes>
-    </LoginGate>
   );
 }
